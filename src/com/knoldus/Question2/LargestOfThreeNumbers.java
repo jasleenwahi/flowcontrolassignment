8515ebbe90f6039640a00b389ab476d7777f5a65
jasleenwahi <jasleen.wahi@knoldus.com>
2023-03-07 15:27:40 +0530

    Commiting my flow control assignment

--- src/com/knoldus/Question2/LargestOfThreeNumbers.java
+++ src/com/knoldus/Question2/LargestOfThreeNumbers.java
@@ -4,7 +4,7 @@ public class LargestOfThreeNumbers
 {
     public static void main(String args[])
     {
-        int number1, number2, number3, result;
+        int number1, number2, number3, result=0;
         Scanner takeInput = new Scanner(System.in);
         System.out.println("Enter 1st number: ");
         number1 = takeInput.nextInt();
@@ -12,7 +12,12 @@ public class LargestOfThreeNumbers
         number2 = takeInput.nextInt();
         System.out.println("Enter 3rd number: ");
         number3 = takeInput.nextInt();
-        result = (number1>number2?number1:number2)>number3?(number1>number2?number1:number2):number3;
+        if (number1 >= number2 && number1 >= number3)
+            result = number1;
+        else if (number2 >= number1 && number2 >= number3)
+            result = number2;
+        else
+            result = number3;
         System.out.println("The largest number is: "+result);
     }
 }
